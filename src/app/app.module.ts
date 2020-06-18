e7cfee1ba182d7dd963bf60d264c4736ffd25fcc
Vincent <chridtinewanja@gmail.com>
2020-06-18 11:45:48 +0300

    This adds the components essentials for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MyProfileComponent } from './components/my-profile/my-profile.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RepoResultComponent } from './components/repo-result/repo-result.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MyProfileComponent,
+    PageNotFoundComponent,
+    RepoResultComponent,
+    SearchResultComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/repo-result/repo-result.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RepoResultComponent } from './repo-result.component';
+
+describe('RepoResultComponent', () => {
+  let component: RepoResultComponent;
+  let fixture: ComponentFixture<RepoResultComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RepoResultComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RepoResultComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
