54719784d99df32d21c49a949c256588a303b6ec
Vincent <chridtinewanja@gmail.com>
2020-06-19 15:45:58 +0300

    This adds a custom pipe for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { MyProfileComponent } from './components/my-profile/my-profile.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { RepoResultComponent } from './components/repo-result/repo-result.component';
-import { SearchResultComponent } from './components/search-result/search-result.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomeComponent } from './components/home/home.component';
+import { SearchResultComponent } from './components/searchResult/searchResult.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MyProfileComponent } from './components/my-profile/my-profile.component';
 import { DaysAgoPipe } from './pipes/days-ago.pipe';
+import { RepoResultComponent } from './components/repo-result/repo-result.component';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    MyProfileComponent,
-    PageNotFoundComponent,
-    RepoResultComponent,
-    SearchResultComponent,
     ToolbarComponent,
-    DaysAgoPipe
+    HomeComponent,
+    SearchResultComponent,
+    PageNotFoundComponent,
+    MyProfileComponent,
+    DaysAgoPipe,
+    RepoResultComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatInputModule,
+    HttpClientModule,
+    MatProgressBarModule,
+    FormsModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
