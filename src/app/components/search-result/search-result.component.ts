4ca3f890ca23618c3d85cccd9557e9f01a8a1ab2
Vincent <chridtinewanja@gmail.com>
2020-06-19 15:26:24 +0300

    This adds the function to be able to search

--- src/app/components/search-result/search-result.component.ts
+++ src/app/components/search-result/search-result.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Users } from '../../models/users';
+import { SearchGitService } from '../../services/search-git.service';
+import { Repositories } from 'src/app/models/repositories'; 
+import { ActivatedRoute, Router } from '@angular/router';
+import { RepositoriesByName } from '../../models/repositories-by-name';
 
 @Component({
-  selector: 'app-search-result',
-  templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.css']
+  selector: 'app-search',
+  templateUrl: './searchResult.component.html',
+  styleUrls: ['./searchResult.component.css']
 })
 export class SearchResultComponent implements OnInit {
 
-  constructor() { }
+  repos:Repositories[];
+  user:Users;
+  username:string;  
 
-  ngOnInit(): void {
+  constructor( private route: ActivatedRoute, private searchGitService: SearchGitService ) {}
+
+  searchResult(){
+    this.username = this.route.snapshot.paramMap.get('username')
+    this.searchGitService.userInfoRequest(this.username)
+    this.user = this.searchGitService.user
+    this.searchGitService.userRepoRequest(this.username)
+    this.repos =this.searchGitService.repos
   }
 
+  ngOnInit(){
+    this.searchResult()
+  }
 }
